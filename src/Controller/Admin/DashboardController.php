1ff0cd99f592847de41e7198e26b1c6b157b7d46
Larsenik67 <ylachqar@gmail.com>
2022-02-11 23:25:08 +0100

    Ajout du Dashboard et création des CRUDS

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\Menu;
+use App\Entity\Alerte;
+use App\Entity\Reservation;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\Admin\ReservationCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -15,18 +19,17 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
-    }
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
 
-    public function configureDashboard(): Dashboard
-    {
-        return Dashboard::new()
-            ->setTitle('La Perle Bleue');
+        return $this->redirect($routeBuilder->setController(ReservationCrudController::class)->generateUrl());
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Administration');
+        yield MenuItem::linkToCrud('Reservation', 'fa fa-file-pdf', Reservation::class);
+        yield MenuItem::linkToCrud('Alerte', 'fa fa-file-pdf', Alerte::class);
+        yield MenuItem::linkToCrud('Menu', 'fa fa-file-pdf', Menu::class);
     }
+    
 }
